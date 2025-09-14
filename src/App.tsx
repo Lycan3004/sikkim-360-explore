48f210601025336058d9302c4700fe66f3edbd73
Shubham Biswal <shubhamkumarbiswal04@gmail.com>
2025-09-14 14:52:34 +0530

    Add Smart Audio Guide route to App component

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ import RichenpongDetails from '@/monasterydetails/richenpong-dt';
 import AbdManuscript from '@/muraldetails/abd-manuscript';
 import SacredMural from '@/muraldetails/sacred-mural';
 import PrayerInsc from '@/muraldetails/prayer-insc';
+import SmartAudioGuide from "@/components/smartAudioBook";
 
 const queryClient = new QueryClient();
 
@@ -31,6 +32,7 @@ const App = () => (
           <Route path="/muraldetails/abd-manuscript" element={<AbdManuscript />} />
           <Route path="/muraldetails/sacred-mural" element={<SacredMural />} />
           <Route path="/muraldetails/prayer-insc" element={<PrayerInsc />} />
+          <Route path="/audio-guide" element={<SmartAudioGuide />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
